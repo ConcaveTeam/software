0df77c1560f45000cd3dbfd31aec47dee9edece0
Kawin Nikomborirak <concavemail@gmail.com>
2018-11-10 01:55:38 +0000

    push tag as well

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+TAG=${TRAVIS_TAG:-latest}
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push $DOCKER_USERNAME/concaveturret
+docker push $DOCKER_USERNAME/concaveturret:$TAG
+
